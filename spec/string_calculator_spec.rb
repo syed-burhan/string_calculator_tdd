01892b70482d046c9a6549c7660e588a4b5b32b2
syed-burhan <syedhsjb@gmail.com>
2024-06-03 19:10:17 +0530

    Fix spec format

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -6,37 +6,37 @@ RSpec.describe StringCalculator do
         it 'returns 0 for an empty string' do
             expect(StringCalculator.add("")).to eq(0)
         end
-    end
 
-    it 'returns the number itself for a single number string' do
-        expect(StringCalculator.add("1")).to eq(1)
-    end
+        it 'returns the number itself for a single number string' do
+            expect(StringCalculator.add("1")).to eq(1)
+        end
 
-    it 'returns sum of two numbers' do
-        expect(StringCalculator.add("1,2")).to eq(3)
-    end
+        it 'returns sum of two numbers' do
+            expect(StringCalculator.add("1,2")).to eq(3)
+        end
 
-    it 'returns sum of multiple numbers' do
-        expect(StringCalculator.add("1,2,3")).to eq(6)
-    end
+        it 'returns sum of multiple numbers' do
+            expect(StringCalculator.add("1,2,3")).to eq(6)
+        end
 
-    it 'does not return incorrect sum' do
-        expect(StringCalculator.add("1,2,3")).not_to eq(10)
-    end
+        it 'does not return incorrect sum' do
+            expect(StringCalculator.add("1,2,3")).not_to eq(10)
+        end
 
-    it 'handles new lines between numbers' do
-        expect(StringCalculator.add("1\n2,3")).to eq(6)
-    end
+        it 'handles new lines between numbers' do
+            expect(StringCalculator.add("1\n2,3")).to eq(6)
+        end
 
-    it 'support different delimiters' do
-        expect(StringCalculator.add("//;\n1;2")).to eq(3)
-    end
+        it 'support different delimiters' do
+            expect(StringCalculator.add("//;\n1;2")).to eq(3)
+        end
 
-    it 'raise an exception for negetive number' do
-        expect{ (StringCalculator.add("1,2,3,-4")) }.to raise_error("Negetive numbers not allowed: -4")
-    end
+        it 'raise an exception for negetive number' do
+            expect{ (StringCalculator.add("1,2,3,-4")) }.to raise_error("Negetive numbers not allowed: -4")
+        end
 
-    it 'raise an exception for multiple negetive numbers' do
-        expect{ (StringCalculator.add("1,2,3,-4,-6")) }.to raise_error("Negetive numbers not allowed: -4, -6")
+        it 'raise an exception for multiple negetive numbers' do
+            expect{ (StringCalculator.add("1,2,3,-4,-6")) }.to raise_error("Negetive numbers not allowed: -4, -6")
+        end
     end
 end

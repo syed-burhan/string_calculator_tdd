b8f60b811c313548ad543e5ba38ced229f152187
syed-burhan <syedhsjb@gmail.com>
2024-06-03 17:45:39 +0530

    Add test cases for sum of multiple numbers and validation

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -15,4 +15,12 @@ RSpec.describe StringCalculator do
     it 'returns sum of two numbers' do
         expect(StringCalculator.add("1,2")).to eq(3)
     end
+
+    it 'returns sum of multiple numbers' do
+        expect(StringCalculator.add("1,2,3")).to eq(6)
+    end
+
+    it 'does not return incorrect sum' do
+        expect(StringCalculator.add("1,2,3")).not_to eq(10)
+    end
 end
